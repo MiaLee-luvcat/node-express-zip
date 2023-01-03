1084a28fe97a446d818b6a131ea241dbbcb1883e
siri-syl <sylee010101@gmail.com>
2023-01-03 22:39:27 +0900

    UPDATE: express Basic routing

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
 app.listen(port, () => {
   console.log(`start http://localhost:${ port }`)
 })

d2a64959fd1b5c3e047fb7a971a34571f7912b2a
siri-syl <sylee010101@gmail.com>
2023-01-05 20:42:36 +0900

    UPDATE: app.js 대신 index.js 사용 + morgan, route 기본 세팅

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
+//* external
 const express = require('express')
-const app = express()
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+
+//* variable
 const port = 3000
 
+const app = express()
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(morgan('dev'))
+
+//* 서버 도메인을 들키면 안 되므로 아예 404로 넘김
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  return res.status(404).send('')
 })
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
+app.get('/siri', (req, res) => {
+  if(!req.query?.input) return res.status(200).send(`Input query 'input'!  example: localhost:3000/siri?input=ok`)
+  return res.status(200).send(`query.input is ${ req.query?.input }`)
 })
 
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
-})
-
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user')
+app.use((req, res, next) => { //* 없는 라우터 분기 404 처리 부분
+  return res.status(404).send()
 })
 
 app.listen(port, () => {

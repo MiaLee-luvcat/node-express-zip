8b0937198e1d7d366a98d298d066fb75b103f2bc
siri-syl <sylee010101@gmail.com>
2023-01-15 21:31:55 +0900

    UPDATE: mypage.js - route 수정 및 get 컨트롤러 작성

--- controllers/mypage.js
+++ controllers/mypage.js
@@ -1,11 +1,16 @@
 //* external
-
+const path = require('path')
 
 //* internal
 const { cl } = require('./helper')
 
-const get = async () => {
-
+const get = async (req, res, next) => {
+  try {
+    res.render('index', { title: 'Express mypage' })
+  }
+  catch (err) {
+    next()
+  }
 }
 
 module.exports = { get }
--- routes/mypage.js
+++ routes/mypage.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
 const mypageController = require('../controllers/mypage')
-
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express mypage' })
-})
-// router.get('/', mypageController.mypage)
+å
+router.get('/', mypageController.get)
 
 module.exports = router

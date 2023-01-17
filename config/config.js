f62d1aa61780e0ebe762a321293497c3a4b65d08
siri-syl <sylee010101@gmail.com>
2023-01-17 23:37:47 +0900

    UPDATE: sequelize config 세팅

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+module.exports = {
+  'development': {
+    'username': process.env.MYSQL_DATABASE_USERNAME || 'root',
+    'password': process.env.MYSQL_DATABASE_PASSWORD || null,
+    'database': process.env.MYSQL_DATABASE_NAME || 'siri',
+    'host': process.env.MYSQL_DATABASE_HOST || '127.0.0.1',
+    'port': process.env.MYSQL_DATABASE_PORT || '3306',
+    'dialect': 'mysql'
+  },
+  'test': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_test',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
+  },
+  'production': {
+    'username': 'root',
+    'password': null,
+    'database': 'database_production',
+    'host': '127.0.0.1',
+    'dialect': 'mysql'
+  }
+}

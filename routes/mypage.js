c9148bc8a4b82fd54af96ad298c4d235be63fa03
siri-syl <sylee010101@gmail.com>
2023-01-18 21:46:55 +0900

    FIX: 오탈자 수정

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const mypageController = require('../controllers/mypage')
-å
+
 router.get('/', mypageController.get)
 
 module.exports = router

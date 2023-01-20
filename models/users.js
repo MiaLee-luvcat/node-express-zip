56febcdc37382f1449b309662a04508a3b77948b
siri-syl <sylee010101@gmail.com>
2023-01-20 21:48:40 +0900

    UPDATE: user model 컬럼 수정

--- models/users.js
+++ models/users.js
@@ -10,11 +10,26 @@ module.exports = function (sequelize, DataTypes) {
       field: 'password',
       type: DataTypes.STRING(30),
       allowNull: false
+    },
+    device: {
+      field: 'device',
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    ip: {
+      field: 'ip',
+      type: DataTypes.STRING(20),
+      allowNull: false
+    },
+    lastLogin: {
+      field: 'lastLogin',
+      type: DataTypes.BIGINT,
+      allowNull: false
     }
   }, {
     underscored: true,
     freezeTableName: true,
     tableName: 'user'
-  });
+  })
   return user
 }

69a33b5b5f1074b4ed48f573075619f66b69288b
siri-syl <sylee010101@gmail.com>
2023-02-26 23:28:12 +0900

    UPDATE: chat-server.node.mjs code convention 맞춤

--- websocket-serve/chat-server.node.mjs
+++ websocket-serve/chat-server.node.mjs
@@ -1,52 +1,52 @@
 // See ./README.md for instructions on how to run this benchmark.
-const port = process.env.PORT || 4001;
-const CLIENTS_TO_WAIT_FOR = parseInt(process.env.CLIENTS_COUNT || "", 10) || 16;
+const port = process.env.PORT || 4001
+const CLIENTS_TO_WAIT_FOR = parseInt(process.env.CLIENTS_COUNT || '', 10) || 16
 
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-var WebSocketServer = require("ws").Server,
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+var WebSocketServer = require('ws').Server,
   config = {
-    host: "0.0.0.0",
+    host: '0.0.0.0',
     port,
   },
   wss = new WebSocketServer(config, function () {
-    console.log(`Waiting for ${CLIENTS_TO_WAIT_FOR} clients to connect..`);
-  });
+    console.log(`Waiting for ${CLIENTS_TO_WAIT_FOR} clients to connect..`)
+  })
 
-var clients = [];
+var clients = []
 
-wss.on("connection", function (ws, { url }) {
-  const name = new URL(new URL(url, "http://localhost:3000")).searchParams.get(
-    "name"
-  );
+wss.on('connection', function (ws, { url }) {
+  const name = new URL(new URL(url, 'http://localhost:3000')).searchParams.get(
+    'name'
+  )
   console.log(
     `${name} connected (${CLIENTS_TO_WAIT_FOR - clients.length} remain)`
-  );
-  clients.push(ws);
+  )
+  clients.push(ws)
 
-  ws.on("message", function (message) {
-    const out = `${name}: ${message}`;
+  ws.on('message', function (message) {
+    const out = `${name}: ${message}`
     for (let client of clients) {
-      client.send(out);
+      client.send(out)
     }
-  });
+  })
 
   // when a connection is closed
-  ws.on("close", function (ws) {
-    clients.splice(clients.indexOf(ws), 1);
-  });
+  ws.on('close', function (ws) {
+    clients.splice(clients.indexOf(ws), 1)
+  })
 
   if (clients.length === CLIENTS_TO_WAIT_FOR) {
-    sendReadyMessage();
+    sendReadyMessage()
   }
-});
+})
 
 function sendReadyMessage() {
-  console.log("All clients connected");
+  console.log('All clients connected')
   setTimeout(() => {
-    console.log("Starting benchmark");
+    console.log('Starting benchmark')
     for (let client of clients) {
-      client.send(`ready`);
+      client.send(`ready`)
     }
-  }, 100);
+  }, 100)
 }
